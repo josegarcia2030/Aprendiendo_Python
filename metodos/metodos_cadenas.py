a04210d4ee28712167071d6271cc7b71005c11ba
jose enrique garcia flores <67805028+josegarcia2030@users.noreply.github.com>
2023-04-15 11:40:10 -0400

    Terminamos con los metodos de cadenas

--- metodos/metodos_cadenas.py
+++ metodos/metodos_cadenas.py
@@ -1,6 +1,6 @@
 #variables
 
-cadena1 = "Hola soy Dalto"
+cadena1 = "Hola,soy,Dalto"
 cadena2 = "Bienvenido maquinola"
 
 resultado = dir(cadena1)
@@ -27,4 +27,47 @@ print(busqueda_find)
 
 #buscar una cadena en otra cadena lanza una excepcion
 busqueda_index = cadena2.index("m")
-print(busqueda_index)
+print(busqueda_index) 
+
+
+#Nos devuelve true si es numerico
+is_numerico = cadena1.isnumeric()
+print(is_numerico)
+
+#Si es alfanumercio devuelve True
+is_alfa_numerico = cadena1.isalpha()
+print(is_alfa_numerico)
+
+#buscamos una cadena en otra cadena, devuelve la cantidad de veces la coincidencia
+contar_coincidencia = cadena1.count("a")
+print(contar_coincidencia)
+
+
+#contamos cuantos caracteres tiene una cadena
+contar_caracteres = len(cadena1)
+print(contar_caracteres)
+
+
+
+#verificamos si una cadena empieza con otra cadena dada
+empieza_con = cadena1.startswith("H")
+print(empieza_con)
+
+
+#verificams si termina con una cadena dada
+termina_con = cadena1.endswith("o")
+print(termina_con)
+
+
+#reemplza una parte de la cadena, por otra
+print(cadena1)
+cadena_nueva = cadena1.replace(",", " ")
+print(cadena_nueva)
+
+
+#separa cadena con la cadena que le pasemos
+cadena_separada = cadena1.split(",")
+print(cadena_separada)
+print(type(cadena_separada))
+for cadena in cadena_separada:
+    print(cadena)
