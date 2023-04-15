895b6a53521c83f22d833191e6a4cc22edbeb2d6
jose enrique garcia flores <67805028+josegarcia2030@users.noreply.github.com>
2023-04-15 12:56:31 -0400

    Se agrego los inputs

---
+++ inputs/input.py
@@ -0,0 +1,12 @@
+
+#pedirle un datos al usuario
+nombre = input("Escribe tu nombre\n")
+edad = input("Escribe tu edad\n")
+print(f'Bienvenido {nombre} , tiene la edad : {edad} años')
+
+
+#pedir un numero al usuario y multiplicarlo por 2
+cantidad_uno = int(input("Escribe un numero\n"))
+print(cantidad_uno * 2)
+
+
